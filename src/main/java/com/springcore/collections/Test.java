781022b1c0da42bf01152e5ec875f6a187e89998
prajwalsharmaa <prajwalst1213@gmail.com>
2026-04-06 20:49:10 +0545

    configuration changes

--- src/main/java/com/springcore/collections/Test.java
+++ src/main/java/com/springcore/collections/Test.java
@@ -1,10 +1,11 @@
 package com.springcore.collections;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/collections/collectionconfig.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/collections/collectionconfig.xml");
 		
 		Emp emp1 = (Emp) context.getBean("emp1");
 		
