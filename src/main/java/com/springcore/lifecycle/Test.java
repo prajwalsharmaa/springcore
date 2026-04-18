efe6a38ec4ba33265806e2eb9beb46709a1d60dc
prajwalsharmaa <prajwalst1213@gmail.com>
2026-04-18 23:11:45 +0545

    bean life cycle method using annotations

--- src/main/java/com/springcore/lifecycle/Test.java
+++ src/main/java/com/springcore/lifecycle/Test.java
@@ -10,17 +10,20 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/lifecycle/lconfig.xml");
-		
-		Samosa s1 = (Samosa) context.getBean("s1");
-		System.out.println(s1);
-		
-		//registering shutdown hook
-		
 		context.registerShutdownHook();
+		/*
+		 * Samosa s1 = (Samosa) context.getBean("s1"); System.out.println(s1);
+		 * 
+		 * //registering shutdown hook
+		 * 
+		 * 
+		 * 
+		 * System.out.println("---------------------------------------"); Pepsi p1 =
+		 * (Pepsi) context.getBean("p1"); System.out.println(p1);
+		 */
+		Example example = (Example)context.getBean("example");
 		
-		System.out.println("---------------------------------------");
-		Pepsi p1 = (Pepsi) context.getBean("p1");
-		System.out.println(p1);
+		System.out.println(example);
 	}
 
 }
