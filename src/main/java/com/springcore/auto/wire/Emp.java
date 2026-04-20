dda5485f4dcd224fdfe85efa2a05b6e990d08b54
prajwalsharmaa <prajwalst1213@gmail.com>
2026-04-20 22:53:11 +0545

    Autowiring using "Autowired" and "Qualifier" annotations

--- src/main/java/com/springcore/auto/wire/Emp.java
+++ src/main/java/com/springcore/auto/wire/Emp.java
@@ -1,6 +1,11 @@
 package com.springcore.auto.wire;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Emp {
+	@Autowired
+	@Qualifier("address1")
 	private Address address;
 
 	public Address getAddress() {
