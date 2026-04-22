99c52ed55f482d2e8a682212516d16a2bb86f32d
prajwalsharmaa <prajwalst1213@gmail.com>
2026-04-23 00:53:01 +0545

    standalone collections

---
+++ src/main/java/com/springcore/standalone/collections/Person.java
@@ -0,0 +1,37 @@
+package com.springcore.standalone.collections;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+public class Person {
+	private List<String> friends;
+	private Map<String, Integer> feestructure;
+	private Properties property;
+	public Properties getProperty() {
+		return property;
+	}
+	public void setProperty(Properties property) {
+		this.property = property;
+	}
+	public List<String> getFriends() {
+		return friends;
+	}
+	public void setFriends(List<String> friends) {
+		this.friends = friends;
+	}
+	public Map<String, Integer> getFeestructure() {
+		return feestructure;
+	}
+	public void setFeestructure(Map<String, Integer> feestructure) {
+		this.feestructure = feestructure;
+	}
+	@Override
+	public String toString() {
+		return "Person [friends=" + friends + ", feestructure=" + feestructure + ", property=" + property + "]";
+	}
+	
+	
+	
+	
+}
