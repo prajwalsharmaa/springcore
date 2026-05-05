7231fa4fce17d042483f40139c8ceeb3920cc9c1
prajwalsharmaa <prajwalst1213@gmail.com>
2026-05-05 07:45:42 +0545

    annotation for collections

--- src/main/java/com/springcore/stereotype/Student.java
+++ src/main/java/com/springcore/stereotype/Student.java
@@ -1,14 +1,22 @@
 package com.springcore.stereotype;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("ob")
+@Scope("prototype")
 public class Student {
 	@Value("Prajwal Sharma")
 	private String studentName;
 	@Value("Bhairahawa")
 	private String city;
+	
+	@Value("#{temp}")
+	public List<String> address;
+	
 	public String getStudentName() {
 		return studentName;
 	}
--- src/main/java/com/springcore/stereotype/Test.java
+++ src/main/java/com/springcore/stereotype/Test.java
@@ -8,8 +8,20 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/stereotype/stereoconfig.xml");
-		Student student = (Student)context.getBean("ob");
-		System.out.println(student);
+		Student student = (Student)context.getBean("ob",Student.class);
+		/*
+		 * System.out.println(student); System.out.println(student.address);
+		 * System.out.println(student.address.getClass());
+		 */
+		
+		System.out.println(student.hashCode());
+		
+		Student student1 = (Student) context.getBean("ob",Student.class);
+		System.out.println(student1.hashCode());//By default spring creates singleton bean so the hashcode will be same which means its the same bean created before
+		
+		Teacher t1 = (Teacher) context.getBean("teacher",Teacher.class);
+		Teacher t2 = (Teacher) context.getBean("teacher",Teacher.class);
+		System.out.println(t1.hashCode() + "  "+ t2.hashCode());
 	}
 
 }
